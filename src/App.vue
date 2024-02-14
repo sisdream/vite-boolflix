462cbd1f40afb49728696dbc010c7137064974e7
sisdream <sissipascale2@gmail.com>
2024-02-14 16:59:28 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script >
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  data(){
+    return{
+      
+    }
+  }
+};
+
+components: { AppHeader, AppMain, AppFooter}
+</script>
+
+<template>
+  <h1>ciaoooo</h1>
+</template>
+
+<style >
+
+</style>
