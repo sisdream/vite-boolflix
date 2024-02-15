1a7c175e3bc09f43fe1d1396f707c57049201fe9
sisdream <sissipascale2@gmail.com>
2024-02-15 14:50:30 +0100

    feat:flag

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,6 +7,15 @@ export default{
             store,
         }
     },
+    methods:{
+        getFlag(langCode){
+            if(langCode == 'it')return new URL('/bandiera-ita.jpg', import.meta.url).href; 
+            if(langCode == 'en')return new URL('/bandiera-eng.jpg', import.meta.url).href;
+            if(langCode == 'es')return new URL('/bandiera-spn.jpg', import.meta.url).href;
+            if(langCode == 'fr')return new URL('/bandiera-fra.jpg', import.meta.url).href;
+            return '/bandiera-vuota.jpg';
+        }
+    }
 }
 </script>
 
@@ -15,12 +24,20 @@ export default{
         <li v-for="movie in store.movies">
           <h2>Titolo: {{ movie.title }}</h2>
           <h5>Titolo originale: {{ movie.original_title }}</h5>
-          <span>Lingua originale: {{ movie.original_language }}</span>
+          <span class="flag">Lingua: 
+            <img :src="getFlag(movie.original_language)" alt="">
+            </span>
           <div>Voto: {{ movie.vote_average }} / 10</div>
+          <div>immagine:<img :src="movie.poster_path" alt=""></div>
         </li>
       </ul>
 </template>
 
-<style>
-
+<style lang="scss">
+    .flag{
+        display: flex;
+        gap: 1rem;
+        margin: 5px;
+        height: 25px;
+    }
 </style>
